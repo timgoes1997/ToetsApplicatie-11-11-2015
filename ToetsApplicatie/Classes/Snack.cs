41d1cb924a49134bd00b4cb3a1ff3e76d58ba390
Ufkillsh0t <Tim_Goes4@hotmail.nl>
2015-11-11 14:34:28 +0100

    new file:   ToetsApplicatie/.gitattributes 	new file:   ToetsApplicatie/.gitignore 	new file:   ToetsApplicatie/App.config 	new file:   ToetsApplicatie/Classes/Administratie.cs 	new file:   ToetsApplicatie/Classes/BTWTarief.cs 	new file:   ToetsApplicatie/Classes/Feestzaal.cs 	new file:   ToetsApplicatie/Classes/Frisdrank.cs 	new file:   ToetsApplicatie/Classes/IInkomsten.cs 	new file:   ToetsApplicatie/Classes/Snack.cs 	new file:   ToetsApplicatie/Classes/Sportzaal.cs 	new file:   ToetsApplicatie/Classes/Sterkedrank.cs 	new file:   ToetsApplicatie/Classes/Vergaderzaal.cs 	new file:   ToetsApplicatie/Classes/Verhuur.cs 	new file:   ToetsApplicatie/Classes/Verkoop.cs 	new file:   ToetsApplicatie/Form1.Designer.cs 	new file:   ToetsApplicatie/Form1.cs 	new file:   ToetsApplicatie/Form1.resx 	new file:   ToetsApplicatie/Program.cs 	new file:   ToetsApplicatie/Properties/AssemblyInfo.cs 	new file:   ToetsApplicatie/Properties/Resources.Designer.cs 	new file:   ToetsApplicatie/Properties/Resources.resx 	new file:   ToetsApplicatie/Properties/Settings.Designer.cs 	new file:   ToetsApplicatie/Properties/Settings.settings 	new file:   ToetsApplicatie/ToetsApplicatie.csproj

---
+++ ToetsApplicatie/Classes/Snack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToetsApplicatie
+{
+    public class Snack : Verkoop
+    {
+        public BTWTarief BTWTarief { get { return BTWtarief; } }
+        public decimal Prijs { get { return prijs; } }
+
+        private BTWTarief BTWtarief;
+        private decimal prijs;
+
+        public Snack(int aantal)
+            : base(aantal)
+        {
+            this.BTWtarief = BTWTarief.Laag;
+            this.prijs = Convert.ToDecimal(3);
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " | " + BTWTarief.ToString() + " | " + base.ToString();
+        }
+    }
+}
---
+++ ToetsApplicatie/Classes/Sterkedrank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToetsApplicatie
+{
+    public class Sterkedrank : Verkoop
+    {
+        public BTWTarief BTWTarief { get { return BTWtarief; } }
+        public decimal Prijs { get { return prijs; } }
+
+        private BTWTarief BTWtarief;
+        private decimal prijs;
+
+        public Sterkedrank(int aantal)
+            : base(aantal)
+        {
+            this.BTWtarief = BTWTarief.Hoog;
+            this.prijs = Convert.ToDecimal(10);
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " | " + BTWTarief.ToString() + " | " + base.ToString();
+        }
+    }
+}
